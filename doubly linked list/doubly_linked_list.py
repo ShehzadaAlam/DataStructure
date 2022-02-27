cc24f4bc18b215768e68ed21b1c7e20843ab8628
ShehzadaAlam <shaz-z@outlook.com>
2022-02-27 23:56:19 +0530

    changes have been made

--- doubly linked list/doubly_linked_list.py
+++ doubly linked list/doubly_linked_list.py
@@ -4,54 +4,34 @@ class Node:
         self.next = None
         self.previous = None
 
-    def __repr__(self):
-        return self.data
-
 
 class DoublyLinkedList:
-    def __init__(self, nodes):
-        self.head = None
-        if nodes is not None:
-            node = Node(data=nodes.pop(0))
-            self.head = node
-            for elem in nodes:
-                newnode = Node(data=elem)
-                node.next = newnode
-                newnode.previous = node
-                node = node.next
+    def __init__(self, data_node):
+        if data_node is not None:
+            new_node = Node(data=data_node.pop(0))
+            self.head = new_node
+            for data in data_node:
+                current_node = Node(data)
+                new_node.next = current_node
+                current_node.previous = new_node
+                new_node = new_node.next
 
     def add_first(self, node):
         if not self.head:
-            self.head = node
-        self.head.previous = node
+            self.head = None
+            return
         node.next = self.head
+        self.head.previous = node
         self.head = node
 
     def add_last(self, node):
         if not self.head:
-            self.head = node
+            self.head = None
+            return
         for current_node in self:
             pass
         current_node.next = node
         node.previous = current_node
-
-    def add_after(self, target_node_data, new_node):
-        if not self.head:
-            raise Exception("Linked list is empty")
-        for current_node in self:
-            if current_node.data == target_node_data:
-                new_node.next = current_node.next
-                new_node.prevous = current_node
-                current_node.next = new_node
-                return
-
-    def __len__(self):
-        node = self.head
-        count = 0
-        while node is not None:
-            count = count + 1
-            node = node.next
-        return count
 
     def __iter__(self):
         node = self.head
@@ -68,3 +48,10 @@ class DoublyLinkedList:
         nodes.append("None")
         return " <-> ".join(nodes)
 
+    def __len__(self):
+        node = self.head
+        count = 0
+        while node is not None:
+            count = count + 1
+            node = node.next
+        return count
--- doubly linked list/implementng_doubly_linkedlist.py
+++ doubly linked list/implementng_doubly_linkedlist.py
@@ -1,18 +1,18 @@
 from doubly_linked_list import *
 
-datastr = ["mango", "raspberry"]
+datastr = ["mango", "raspberry", "grapes"]
 dll = DoublyLinkedList(datastr)
 print(dll)
 print(len(dll))
 
-dll.add_first(Node("grapes"))
+dll.add_first(Node("pineapple"))
 print(dll)
 # print(dll.head.next.next)
 
 dll.add_last(Node("banana"))
 print(dll)
 
-dll.add_after("raspberry", Node("fig"))
-print(dll)
-print(dll.head.next.next.next.previous)
+# dll.add_after("raspberry", Node("fig"))
+# print(dll)
+# print(dll.head.next.next.next.previous)
 
