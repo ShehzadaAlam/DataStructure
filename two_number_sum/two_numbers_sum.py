a0ea76ff98c7204822fc2a548c5a29311ac2fef7
ShehzadaAlam <shaz-z@outlook.com>
2022-04-02 21:04:28 +0530

    two number sum file added

---
+++ two_number_sum/two_numbers_sum.py
@@ -0,0 +1,16 @@
+strarray = [3, 6, 7, 9, 23, 67, 10]
+target = 16
+
+
+def find_sum(strarray, target):
+    prev = {}
+    for index, value in enumerate(strarray):
+        diff = target - value
+        if diff in prev:
+            return [prev[diff], index]
+        prev[value] = index
+    return
+
+
+print(find_sum(strarray, target))
+
